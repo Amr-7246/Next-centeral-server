1773950f0d3e07d1c3bce698bbc26a72d89ad692
Amr Ehab <aehab7246@gmail.com>
2025-08-15 19:51:51 +0300

    Done

--- src/components/Working.tsx
+++ src/components/Working.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Working = () => {
   return (
-    <div className='flex-center absolute top-0 h-full w-full bg-[#000000e8] z-5 backdrop-blur-lg '>
+    <div className='flex-center absolute top-0 h-full w-full bg-[#000000e8] z-5 backdrop-blur-sm '>
       <h1 className='font-black text-text text-[40px]'>Comming Soon . . . </h1>
     </div>
   )
